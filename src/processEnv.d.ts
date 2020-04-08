c90c56343218d138186218001f637e5734464caf
Jakub Kohoutek <jakub.kohoutek@merck.com>
2020-04-08 18:16:16 +0200

    Add typing for environment variables

---
+++ src/processEnv.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    PORT?: string;
+    ACCESS_TOKEN?: string;
+    AWS_ACCESS_KEY_ID: string;
+    AWS_SECRET_ACCESS_KEY: string;
+    AWS_DEFAULT_REGION: string;
+  }
+}
